473fb44c44d4073b85deae99710121d140202673
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-15 21:14:32 -0400

    Added update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,51 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://marty:00Zind00@ds231951.mlab.com:31951/zindel', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('There was a connection error :( ');
+    } else {
+        console.log('Connected to the DB!');
+        let db = client.db('zindel');
+
+        // deleteMany
+        // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+        //     console.log(result);
+        // });
+
+
+        // deleteOne
+        // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+
+        //     //console.log(result);
+        //     if (result.CommandResult.result.n > 0)
+        //     {
+        //         console.log('Deleted one!!!');
+        //     } else {
+        //         console.log('None found!');
+        //     }})};
+
+        // findOneAndDelete
+        // db.collection('Todos').findOneAndDelete({text: 'Eat lunch'}).then((result) => {
+        //     console.log(result);
+        // });
+
+        // db.collection('Todos').findOneAndUpdate({text: 'Eat lunch'}, {
+        //     $set: {
+        //         completed: true
+        //     }}, {
+        //         returnOriginal: false
+        //     }).then((result) => {
+        //     console.log(result);
+        // });
+
+        db.collection('Users').findOneAndUpdate({name: 'Jeanne'}, {
+            $set: {
+                name: 'Stephna'
+            }, $inc: {age: 1}}, {
+                returnOriginal: false
+            }).then((result) => {
+            console.log(result);
+        });
+            
+            //db.close();
+    }});
