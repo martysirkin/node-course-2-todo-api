31590f986e94ab76fd8be20d5b1c13b7fa9bea59
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-19 11:13:45 -0400

    Fix environment variable for Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://marty:00Zind00@ds231951.mlab.com:31951/zindel'; 
-} else if (env === 'test') {
+if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://marty:00Zind00@ds143511.mlab.com:43511/zindeltest';
+} else if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://marty:00Zind00@ds231951.mlab.com:31951/zindel'; 
+} else 
+{
+  process.env.MONGODB_URI = 'mongodb://marty:00Zind00@ds231951.mlab.com:31951/zindel'; 
 }
