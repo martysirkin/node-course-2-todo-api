07858927373af93459aecddea73b0902f6943f3f
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-28 18:42:59 -0400

    Added tests for login

--- server/server.js
+++ server/server.js
@@ -113,8 +113,6 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
-
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -217,7 +217,7 @@ describe('POST /users', () => {
                     expect(user).toBeTruthy();
                      //expect(user.password).toNotEqual(password);
                     done();
-                });
+                }).catch((e) => done(e));
             });
         });
 
@@ -239,4 +239,55 @@ describe('POST /users', () => {
             .expect(400)
             .end(done);
     });
+});
+
+describe('POST /users/login', () => {
+    it('should succeed if valid user/password provided', (done) => {
+         request(app)
+             .post('/users/login')
+             .send({
+                 email: users[1].email,
+                 password: users[1].password
+             })
+             .expect(200)
+             .expect((res) => {
+                 expect(res.headers['x-auth']).toBeTruthy();
+             })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    //console.log(user);
+                    //console.log(user.tokens[0].access);
+                    expect(user.tokens[0].access).toEqual('auth')
+                    expect(user.tokens[0].token).toEqual(res.headers['x-auth'])               
+                    done();
+                }).catch((e) => done(e));
+              });
+      });
+        
+
+    it('should fail login with invalid parameters', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + 'k'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toBeFalsy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                     expect(user.tokens.length).toBe(0);          
+                    done();
+                }).catch((e) => done(e));
+              });    });
 });
